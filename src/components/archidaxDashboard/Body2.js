b29c252a0481d0d9f63d3d24f09799167a7bf008
hesa <hesamilan@gmail.com>
2020-05-16 05:38:29 +0700

    push

--- src/components/archidaxDashboard/Body2.js
+++ src/components/archidaxDashboard/Body2.js
@@ -1,10 +1,5 @@
 import React from "react";
 
-import BuySellBPS from "./body2/BuySellBPS";
-import MarketCapCard from "./body2/MarketCapCard";
-import Stock from "./body2/Stock";
-import Chart from "./body1/Chart";
-
 export default function Body2() {
   return (
     <div className="row m-0 p-3 position-relative top-50">
